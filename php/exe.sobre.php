30186b6d7349379b0c31e46c48f7ce1fc399e63f
RafaelCavallin <rafael_cavallin@hotmail.com>
2018-07-26 23:13:04 -0300

    Signed-off-by: RafaelCavallin <rafael_cavallin@hotmail.com>

--- php/exe.sobre.php
+++ php/exe.sobre.php
@@ -1,37 +1,36 @@
 <section id="sobre">
-
 	<div class="container mt-4">
 		
 		<div class="row">
 			<div class="col-md-12 mb-4">
 				<h4 class="display-4">Sobre</h5>
-			</div>
-		</div>
+       </div>
+     </div>
 
-		<div class="row">
-			<div class="col-12">
-				<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio natus, sapiente explicabo, harum vero aliquam facilis ratione repudiandae provident in ab necessitatibus autem, quaerat obcaecati qui, consectetur quidem cupiditate architecto.</p>
-			</div>
-		</div>
-
-		<div class="row">
-            <div class="col-sm-12 col-md-6 d-none d-md-block">
-                <img src="img/c.png" alt="" class="img-fluid mb-3 rounded-circle"> 
-            </div>
-            <div class="col-sm-12 col-md-6">
-                <h1 class="display-4 mb-4 text-center">Ferramentas</h1>
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
-          </div>
+     <div class="row">
+       <div class="col-12">
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio natus, sapiente explicabo, harum vero aliquam facilis ratione repudiandae provident in ab necessitatibus autem, quaerat obcaecati qui, consectetur quidem cupiditate architecto.</p>
       </div>
+    </div>
 
-      <div class="row">
-      	<div class="col-12">
-      		<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
-      		<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
-      	</div>
+    <div class="row">
+      <div class="col-sm-12 col-md-6 d-none d-md-block">
+        <img src="img/c.png" alt="" class="img-fluid mb-3 rounded-circle"> 
       </div>
+      <div class="col-sm-12 col-md-6">
+        <h1 class="display-4 mb-4 text-center">Ferramentas</h1>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
+      </div>
+    </div>
 
-	</div>
+    <div class="row">
+     <div class="col-12">
+      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
+      <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique iusto, soluta labore in, velit ducimus quisquam qui blanditiis optio, libero natus est nulla expedita neque? A ab eius quaerat animi, ea, amet est ratione, necessitatibus nesciunt voluptates vitae laudantium numquam aliquid odit recusandae iste ipsam aspernatur sapiente id ut et.</p>
+     </div>
+    </div>
+
+  </div>
 </section>
