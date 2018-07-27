f2e8fcc2219aaa8f42cd28b4931cbb8231b69822
RafaelCavallin <rafael_cavallin@hotmail.com>
2018-07-26 22:39:44 -0300

    Commit do dia 26/07/2018

--- php/exe.footer.php
+++ php/exe.footer.php
@@ -1,5 +1,5 @@
 <!-- FOOTER -->
-<footer id="main-footer" class="bg-dark text-white">
+<footer id="main-footer" class="bg-dark text-white mt-3">
 	<div class="container py-3">
 		<div class="row">
 			<div class="col-sm-6 text-center">
